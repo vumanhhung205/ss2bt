4a82cfe85e30f544cfb170b8a35d9f44bbf739ef
vumanhhung205 <142978260+vumanhhung205@users.noreply.github.com>
2023-11-16 12:56:41 +0700

    Add files via upload

---
+++ ss2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int edge=4;
+	int long_edge=6;
+	int short_edge=3;
+	const float PI=3.14;
+	int radius=3;
+	int perimeter_chunhat=(long_edge+short_edge)*2;
+	int perimeter_tron=2*PI*radius;
+	int acreage_chunhat=long_edge*short_edge;
+	int acreage_tron=PI*radius*radius;
+	}
